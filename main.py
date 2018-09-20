73dc70a47cbe44b56809aae6fb98824c0f5f07a7
Karanraj Chauhan <chauhank@bu.edu>
2018-09-20 13:07:37 -0400

    submission commit

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from Datamodel import Camera, Detector
 
 def get_cmd_args():
     parser = argparse.ArgumentParser(description = 'Senior Design Mini Project for Car Frequency Monitoring')
-    parser.add_argument('--type', type = str, nargs = 1, default = None,
+    parser.add_argument('--type', type = str, default = None,
         help = '\'online\' for directly from camera and \'offline\' for processing from video.'
     )
     parser.add_argument('--path', type = str, nargs = '+', default = None,
         help = 'Path to video file to be used for offline detection.'
     )
-    parser.add_argument('--threshold', type = float, nargs = 1, default = 0.3,
+    parser.add_argument('--threshold', type = float, default = 0.3,
         help = 'Threshold for detection. Min = 0 and Max = 1.'
     )
     parser.add_argument('--width', type = int, nargs = '+', default = 304,
@@ -26,22 +26,17 @@ def get_cmd_args():
     parser.add_argument('--classes', type = int, nargs = '+', default = 3,
         help = 'Class to be detected from the COCO dataset'
     )
-    parser.add_argument('--fps', type = int, nargs = '+', default = 1,
+    parser.add_argument('--fps', type = int, default = 1,
         help = 'Frames per second to be captured by the camera.'
     )
     return parser.parse_args()
 
 def process_detections(detections, threshold, class_num):
-    maxval = -1
     count = 0
     for i in range(1, detections.shape[2]):
         if (detections[0, 0, i, 2]) >= threshold and (int(detections[0, 0, i, 1])) == class_num:
             count += 1
 
-    if (__debug__):
-        if detections[0, 0, i, 2] > maxval:
-                maxval = detections[0, 0, i, 2]
-        print("max value of all detections was {}".format(maxval))
     return count
 
 def load_video(path):
@@ -50,28 +45,31 @@ def load_video(path):
 def main():
     args = get_cmd_args()
     # check for required arguments
-    if (args.type[0] != 'offline' and args.type[0] != 'online'):
+    if (args.type != 'offline' and args.type != 'online'):
         print("Invalid argument for option \'--type\'. Only offline and online are valid options")
         return
     
     # picamera setup
-    if (args.type[0] == 'online'):
+    if (args.type == 'online'):
         camera = Camera(args.width, args.height, args.fps)
     else:
         if (args.path is None):
             print("For offline video processing, a path to a video file must be specified.")
-        video = load_video(args.path)
+        video = load_video(args.path[0])
 
     # detector setup
     detector = Detector(args.width, args.height)
 
     # main detection loop
     try:
+        img_number = 0
         while True:
             
             # run ssd on capture from camera
-            if (args.type[0] == 'online'):
+            if (args.type == 'online'):
                 image = camera.get_frame(args.width, args.height)
+                cv.imwrite('img{}.jpg'.format(img_number), image)
+                img_number += 1
             
             # run ssd on a frame from video
             else:
@@ -87,12 +85,18 @@ def main():
             num_cars = process_detections(detections, args.threshold, args.classes)
             print("{}, {}".format(datetime.datetime.now(), num_cars))
     except KeyboardInterrupt:
-        video.release()
-        camera.close()
+        
+        if args.type == 'offline':
+            video.release()
+        else:
+            camera.close()
     
     # cleanup
     print("Exiting")
-    camera.close()
+    if args.type == 'offline':
+        video.release()
+    else:
+        camera.close()
 
 if __name__ == "__main__":
         main()
